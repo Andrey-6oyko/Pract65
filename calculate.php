9e49fb6cde8483432e7916a601effd2892102f83
Andrey-6oyko <andreyboyko200032@gmail.com>
2024-06-09 18:06:16 +0300

    Second

--- calculate.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $number1 = $_POST['number1'];
-    $number2 = $_POST['number2'];
-    $sum = $number1 + $number2;
-
-    echo "Сума: " . $sum;
-}
-?>
--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function calculateSum(event) {
-    event.preventDefault(); // Запобігає відправленню форми
+function calculateSum() {
+    // Отримання значень чисел з полів введення
     const number1 = parseFloat(document.getElementById('number1').value);
     const number2 = parseFloat(document.getElementById('number2').value);
-    const sum = number1 + number2;
 
-    alert('Сума: ' + sum); // Виведення результату в діалоговому вікні
-    document.getElementById('result').value = sum; // Виведення результату в окремому полі форми
+    // Перевірка, чи введені числа є валідними
+    if (!isNaN(number1) && !isNaN(number2)) {
+        // Обчислення суми
+        const sum = number1 + number2;
+
+        // Виведення результату в діалоговому вікні
+        alert(`Сума: ${sum}`);
+
+        // Відображення результату в окремому полі на сторінці
+        document.getElementById('result').innerText = `Сума: ${sum}`;
+    } else {
+        alert('Будь ласка, введіть валідні числа.');
+    }
 }
